3b6e5fe1c81d18ff76543918276f3d15174a289c
Farhan Alam <115414440+Istaqalam@users.noreply.github.com>
2024-02-06 00:00:00 +0545

    Update: Design improvements and functionality updates

---
+++ CMS/About_Us_Page.vb
@@ -0,0 +1,25 @@
+﻿Public Class About_Us_Page
+    Private Sub About_Us_Page_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Centering the form in the middle of the parent form
+        Dim x As Integer
+        Dim y As Integer
+        Dim r As Rectangle
+
+        If Not Parent Is Nothing Then
+            r = Parent.ClientRectangle
+            x = r.Width - Me.Width + Parent.Left
+            y = r.Height - Me.Height + Parent.Top
+        Else
+            r = Screen.PrimaryScreen.WorkingArea
+            x = r.Width - Me.Width
+            y = r.Height - Me.Height
+        End If
+
+        x = CInt(x / 2)
+        y = CInt(y / 2)
+
+        Me.StartPosition = FormStartPosition.Manual
+        Me.Location = New Point(x, y)
+
+    End Sub
+End Class
