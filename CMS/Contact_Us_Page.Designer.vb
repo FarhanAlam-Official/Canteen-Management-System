9366f476b7574fd92d17db9c125a7db1fce53906
Farhan Alam <115414440+Istaqalam@users.noreply.github.com>
2024-03-21 00:00:00 +0545

    Add: Database query and documentation for project report

---
+++ CMS/Contact_Us_Page.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Contact_Us_Page
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.lblEmailValue = New System.Windows.Forms.Label()
+        Me.lblPhonenoValue = New System.Windows.Forms.Label()
+        Me.lblAddressValue = New System.Windows.Forms.Label()
+        Me.lblOperatinghoursValue = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.lblOperatingHours = New System.Windows.Forms.Label()
+        Me.lblAddress = New System.Windows.Forms.Label()
+        Me.lblPhoneno = New System.Windows.Forms.Label()
+        Me.picboxLogo = New System.Windows.Forms.PictureBox()
+        CType(Me.picboxLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.Transparent
+        Me.lblTitle.Font = New System.Drawing.Font("Monotype Corsiva", 25.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(191, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.lblTitle.Location = New System.Drawing.Point(270, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(201, 51)
+        Me.lblTitle.TabIndex = 5
+        Me.lblTitle.Text = "Contact Us"
+        '
+        'lblMsg
+        '
+        Me.lblMsg.AutoSize = True
+        Me.lblMsg.Font = New System.Drawing.Font("Trebuchet MS", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMsg.ForeColor = System.Drawing.Color.Gray
+        Me.lblMsg.Location = New System.Drawing.Point(248, 78)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(268, 24)
+        Me.lblMsg.TabIndex = 7
+        Me.lblMsg.Text = " We'd love to hear from you!"
+        '
+        'lblEmailValue
+        '
+        Me.lblEmailValue.AutoSize = True
+        Me.lblEmailValue.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmailValue.Location = New System.Drawing.Point(350, 180)
+        Me.lblEmailValue.Name = "lblEmailValue"
+        Me.lblEmailValue.Size = New System.Drawing.Size(272, 28)
+        Me.lblEmailValue.TabIndex = 8
+        Me.lblEmailValue.Text = " cmssupport@gmail.com"
+        '
+        'lblPhonenoValue
+        '
+        Me.lblPhonenoValue.AutoSize = True
+        Me.lblPhonenoValue.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPhonenoValue.Location = New System.Drawing.Point(350, 232)
+        Me.lblPhonenoValue.Name = "lblPhonenoValue"
+        Me.lblPhonenoValue.Size = New System.Drawing.Size(149, 28)
+        Me.lblPhonenoValue.TabIndex = 9
+        Me.lblPhonenoValue.Text = " 9807225586"
+        '
+        'lblAddressValue
+        '
+        Me.lblAddressValue.AutoSize = True
+        Me.lblAddressValue.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAddressValue.Location = New System.Drawing.Point(350, 278)
+        Me.lblAddressValue.Name = "lblAddressValue"
+        Me.lblAddressValue.Size = New System.Drawing.Size(325, 28)
+        Me.lblAddressValue.TabIndex = 10
+        Me.lblAddressValue.Text = " Bharatpur-03,Chitwan,Nepal"
+        '
+        'lblOperatinghoursValue
+        '
+        Me.lblOperatinghoursValue.AutoSize = True
+        Me.lblOperatinghoursValue.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOperatinghoursValue.Location = New System.Drawing.Point(350, 325)
+        Me.lblOperatinghoursValue.Name = "lblOperatinghoursValue"
+        Me.lblOperatinghoursValue.Size = New System.Drawing.Size(224, 28)
+        Me.lblOperatinghoursValue.TabIndex = 11
+        Me.lblOperatinghoursValue.Text = " 6:00 AM - 11:00 PM"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmail.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(191, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.lblEmail.Location = New System.Drawing.Point(135, 180)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(198, 28)
+        Me.lblEmail.TabIndex = 12
+        Me.lblEmail.Text = "Email                 :"
+        '
+        'lblOperatingHours
+        '
+        Me.lblOperatingHours.AutoSize = True
+        Me.lblOperatingHours.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOperatingHours.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(191, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.lblOperatingHours.Location = New System.Drawing.Point(135, 325)
+        Me.lblOperatingHours.Name = "lblOperatingHours"
+        Me.lblOperatingHours.Size = New System.Drawing.Size(209, 28)
+        Me.lblOperatingHours.TabIndex = 13
+        Me.lblOperatingHours.Text = "Operating Hours : "
+        '
+        'lblAddress
+        '
+        Me.lblAddress.AutoSize = True
+        Me.lblAddress.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAddress.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(191, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.lblAddress.Location = New System.Drawing.Point(135, 278)
+        Me.lblAddress.Name = "lblAddress"
+        Me.lblAddress.Size = New System.Drawing.Size(202, 28)
+        Me.lblAddress.TabIndex = 14
+        Me.lblAddress.Text = "Address              :"
+        '
+        'lblPhoneno
+        '
+        Me.lblPhoneno.AutoSize = True
+        Me.lblPhoneno.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPhoneno.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(191, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.lblPhoneno.Location = New System.Drawing.Point(135, 232)
+        Me.lblPhoneno.Name = "lblPhoneno"
+        Me.lblPhoneno.Size = New System.Drawing.Size(199, 28)
+        Me.lblPhoneno.TabIndex = 15
+        Me.lblPhoneno.Text = "Phone no           :"
+        '
+        'picboxLogo
+        '
+        Me.picboxLogo.Image = Global.CMS.My.Resources.Resources.Icon
+        Me.picboxLogo.Location = New System.Drawing.Point(12, 9)
+        Me.picboxLogo.Name = "picboxLogo"
+        Me.picboxLogo.Size = New System.Drawing.Size(116, 93)
+        Me.picboxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picboxLogo.TabIndex = 6
+        Me.picboxLogo.TabStop = False
+        '
+        'Contact_Us_Page
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblPhoneno)
+        Me.Controls.Add(Me.lblAddress)
+        Me.Controls.Add(Me.lblOperatingHours)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblOperatinghoursValue)
+        Me.Controls.Add(Me.lblAddressValue)
+        Me.Controls.Add(Me.lblPhonenoValue)
+        Me.Controls.Add(Me.lblEmailValue)
+        Me.Controls.Add(Me.lblMsg)
+        Me.Controls.Add(Me.picboxLogo)
+        Me.Controls.Add(Me.lblTitle)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Contact_Us_Page"
+        Me.Text = "Contact Us Page"
+        CType(Me.picboxLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents picboxLogo As PictureBox
+    Friend WithEvents lblMsg As Label
+    Friend WithEvents lblEmailValue As Label
+    Friend WithEvents lblPhonenoValue As Label
+    Friend WithEvents lblAddressValue As Label
+    Friend WithEvents lblOperatinghoursValue As Label
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents lblOperatingHours As Label
+    Friend WithEvents lblAddress As Label
+    Friend WithEvents lblPhoneno As Label
+End Class
