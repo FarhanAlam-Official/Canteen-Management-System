6631d6d730fb8b1817e60de69b88ba64d18276e2
Farhan Alam <115414440+Istaqalam@users.noreply.github.com>
2024-04-06 00:00:00 +0545

    Add: Resources including icons, logos, and images

---
+++ CMS/GlobalVariables.vb
@@ -0,0 +1,21 @@
+﻿Module GlobalVariables
+    Public PlateItems As New List(Of PlateItem)
+End Module
+
+Public Class PlateItem
+
+    Public Property ProductId As Integer
+    Public Property Name As String
+
+    Public Property Price As Decimal
+
+    Public Property Quantity As Integer
+
+    Public Property Image As Image
+
+    Public Property Category As String
+
+    Public Property CategoryId As Integer
+
+    Public Property Discount As Decimal
+End Class
